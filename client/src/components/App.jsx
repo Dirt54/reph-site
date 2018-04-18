f44922e66b788a3ea16f0323a8ab481cc5628693
Luke Robinson <lukerobinson@Lukes-MBP-2.lan>
2018-04-18 10:31:44 -0400

    touch up

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import About from "./About";
-import Vision from "./Vision";
-import Mission from "./Mission";
 import Partners from "./Partners";
 import Connect from "./Connect";
 import Footer from "./Footer";
@@ -20,8 +18,6 @@ export default class App extends Component {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
-            <Route exact path="/vision" component={Vision} />
-            <Route exact path="/mission" component={Mission} />
             <Route exact path="/partners" component={Partners} />
             <Route exact path="/connect" component={Connect} />
           </Switch>
