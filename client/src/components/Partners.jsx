1050d6366ff9a1f5620053df7c1c22fdd61ad354
Luke Robinson <lukerobinson@Lukes-MBP-2.lan>
2018-04-20 14:30:23 -0400

    commit

--- client/src/components/Partners.jsx
+++ client/src/components/Partners.jsx
@@ -6,6 +6,20 @@ export default class Home extends Component {
     constructor(props) {
         super(props);
     }
+//     var coll = document.getElementsByClassName("collapsible");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+
     render() {
         return (
             <Fragment>
